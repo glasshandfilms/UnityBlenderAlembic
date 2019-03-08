8bc2b5151192f3d081f58f50711b63aebbc13b39
Brandon <brandon@glasshandstudios.com>
2019-03-07 19:24:37 -0500

    big first push

---
+++ UnityBlenderAlembic/Assets/Scripts/BlenderShapes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlenderShapes : MonoBehaviour
+{
+    SkinnedMeshRenderer thisSkinnedMeshRenderer;
+
+    void Start()
+    {
+        thisSkinnedMeshRenderer = this.gameObject.GetComponent<SkinnedMeshRenderer>();
+        StartCoroutine(ShapeKeyAnimation());
+    }
+
+    
+
+    IEnumerator ShapeKeyAnimation()
+    {
+        for(float i = 0.0f; i<100.0f; i += 1.0f)
+        {
+            thisSkinnedMeshRenderer.SetBlendShapeWeight(0, i);
+            yield return new WaitForSeconds(0.02f);
+        }
+
+        for(float i = 0.0f; i < 100.0f; i += 1.0f)
+        {
+            thisSkinnedMeshRenderer.SetBlendShapeWeight(0, 100 - i);
+            yield return new WaitForSeconds(0.02f);
+        }
+        StartCoroutine(ShapeKeyAnimation());
+    }
+}
